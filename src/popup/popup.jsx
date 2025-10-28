47b4e5dbf6f5ea2ebdc8a9551bbb3d87fe24f10c
Your Name <your-email@example.com>
2025-10-28 20:26:12 +0500

    push pop in our code

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -7,26 +7,54 @@ function popup() {
 
         
 const  [OpenPopup, setOpenPopup]=useState("false")
-const [Password , setPassword]=useState('false')
+const [Show , setShow]= useState(true)
+
+const tooglePassword=() =>{
+  
+  setShow(!Show)
+  }
+  
+  
+
+
+
    
   return (
 
     <div style={{textAlign:'center'}}>
      
-      <button className="pop" style={{textAlign:'center',backgroundColor:'green',padding:'10px',border:'green'}} onClick={()=>setOpenPopup(true)}>Open  Popup</button>
+      <button className="pop" style={{textAlign:'center',backgroundColor:'pink',padding:'10px',border:'green'}} onClick={()=>setOpenPopup(true)}>Open  Popup</button>
         
       {
+        
         OpenPopup &&
-      <div className="uppop" style={{width:'300px',height:'150px',backgroundColor:'#eee', padding:'10px'}}>
+      <div className='super' style={{display:'flex',justifyContent:'center',alignItems:'center',height:'80vh'}}>  
+            <div className="uppop" style={{width:'300px',height:'190px',backgroundColor:'#eee', padding:'10px'}}>
       <div className="popup"style={{display:'flex', justifyContent:'space-between'}}>
-        <h2>PopUp</h2>
+        <h2>Form</h2>
         <button style={{border:'none'}} onClick={()=>setOpenPopup(false)}>X</button>
       </div>
-      {/* <p>Popup message here..</p> */}
-      <div >
-      <input type="name"  placeholder='Name' style={{padding:'0 10px', margin:'10px 0',width:'200px',height:'30px'}}/></div>
-      <input type="password" placeholder='Password' style={{padding:'0 10px',width:'200px',height:'30px'}}/>
+    
+  
+<div className='icons' style={{
+  display:'flex',
+  justifyContent:'center',
+  alignItems:'center',
+  gap:'2px'
+}}>
+      <input type={Show ? "Password" : 'Text'} placeholder='Password' style={{padding:'0 10px',width:'200px',height:'30px',position:'relative'}} />
+       <span className='icon' style={{}}>
+{
+
+          Show ? <FaEye onClick={tooglePassword}/> :<FaEyeSlash onClick={tooglePassword}/>
+}
+</span>
 </div>
+       </div>
+
+      
+</div>
+
 }
     </div>
   )
